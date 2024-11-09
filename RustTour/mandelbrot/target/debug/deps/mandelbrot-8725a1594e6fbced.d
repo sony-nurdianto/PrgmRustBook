c78b478b8e40254719c278147e055969b1b1ce06
sony nurdianto <61543711+sony-nurdianto@users.noreply.github.com>
2024-11-10 05:24:06 +0700

    Delete RustTour/mandelbrot/target directory

--- RustTour/mandelbrot/target/debug/deps/mandelbrot-8725a1594e6fbced.d
+++
@@ -1,5 +0,0 @@
-/home/sony/Learn/Rust/PrgmRustBook/RustTour/mandelbrot/target/debug/deps/libmandelbrot-8725a1594e6fbced.rmeta: src/main.rs
-
-/home/sony/Learn/Rust/PrgmRustBook/RustTour/mandelbrot/target/debug/deps/mandelbrot-8725a1594e6fbced.d: src/main.rs
-
-src/main.rs:
--- RustTour/mandelbrot/target/debug/deps/mandelbrot-e4a9104825697e84.d
+++
@@ -1,5 +0,0 @@
-/home/sony/Learn/Rust/PrgmRustBook/RustTour/mandelbrot/target/debug/deps/libmandelbrot-e4a9104825697e84.rmeta: src/main.rs
-
-/home/sony/Learn/Rust/PrgmRustBook/RustTour/mandelbrot/target/debug/deps/mandelbrot-e4a9104825697e84.d: src/main.rs
-
-src/main.rs:
